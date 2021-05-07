030fc5120f0b78704dce0a7df942f9d2b6d5fad7
sjimpy <sjimpy@users.noreply.github.com>
2021-05-07 19:25:35 +0200

    javascript toegevoegd

--- opdracht1/scripts/script.js
+++ opdracht1/scripts/script.js
@@ -1,2 +1,53 @@
 // JavaScript Document
-console.log("howdy");
+window.addEventListener("resize", checkWindowSize);
+var thunmbsticks = document.querySelectorAll("h2")[1];
+
+checkWindowSize()
+function checkWindowSize() {
+  var secrion2 = document.querySelectorAll("section")[1];
+  var sectionsize = secrion2.offsetWidth;
+
+  secrion2.addEventListener("mousemove", function(obj) {
+    var target = obj.target;
+    var rect = target.getBoundingClientRect();
+
+    var x = obj.clientX - rect.left;
+    var y = obj.clientY - rect.top;
+    xPercentage = x/sectionsize * 100;
+    yPercentage = y/sectionsize * 100;
+
+    if (xPercentage < 30) {
+     if (yPercentage < 30) {
+      thunmbsticks.className = "linksboven";
+     }
+     else if (yPercentage > 60) {
+      thunmbsticks.className = "linksonder";
+     }
+     else {
+      thunmbsticks.className = "linksmidden";
+     }
+    }
+    else if (xPercentage > 60) {
+      if (yPercentage < 30) {
+        thunmbsticks.className = "rechtsboven";
+      }
+      else if (yPercentage > 60) {
+        thunmbsticks.className = "rechtsonder";
+      }
+      else {
+        thunmbsticks.className = "rechtsmidden";
+      }
+    }
+    else {
+      if (yPercentage < 30) {
+        thunmbsticks.className = "middenboven";
+      }
+      else if (yPercentage > 60) {
+        thunmbsticks.className = "middenonder";
+      }
+      else {
+        thunmbsticks.className = "middenmidden";
+      }
+    }
+  })
+}

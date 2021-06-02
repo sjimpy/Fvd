ec05ff59a43c0826563f2ef9dc4aec50c2c3807f
sjimpy <sjimpy@users.noreply.github.com>
2021-06-02 15:14:16 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -41,23 +41,23 @@ function showScore(i) {
   }
 }
 
-
-var c
-let cars = [
-  {
-    "color": "purple",
-    "type": "minivan",
-    "registration": new Date('2017-01-03'),
-    "capacity": 7
-  },
-  {
-    "color": "red",
-    "type": "station wagon",
-    "registration": new Date('2018-03-03'),
-    "capacity": 5
-  },
-  {
-    ...
-  },
-  ...
-]
+//
+// var c
+// let cars = [
+//   {
+//     "color": "purple",
+//     "type": "minivan",
+//     "registration": new Date('2017-01-03'),
+//     "capacity": 7
+//   },
+//   {
+//     "color": "red",
+//     "type": "station wagon",
+//     "registration": new Date('2018-03-03'),
+//     "capacity": 5
+//   },
+//   {
+//     ...
+//   },
+//   ...
+// ]
